8fbea92feac9d8300b2b003893ad3a019d120299
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-02-02 19:13:52 +0700

    fix

--- provisioning_loadbalancer.sh
+++ provisioning_loadbalancer.sh
@@ -15,24 +15,27 @@ sudo cp cert/* /etc/tls
 
 #config nginx
 sudo bash -c 'cat <<EOF > /etc/nginx/conf.d/nginx.conf
-http{
- 
-    upstream finditgeek{
-        baris method : round-robin
-        server 10.10.10.30 max_fails=3 fail_timeout=30s;
-        server 10.10.10.30 max_fails=3 fail_timeout=30s;
-    }
+upstream finditgeek {
+    server 10.10.10.20:80 max_fails=3 fail_timeout=30s;
+    server 10.10.10.30:80 max_fails=3 fail_timeout=30s;
+}
 
-    server {
-        listen 443 ssl;
-        ssl_certificate /etc/tls/cert.pem
-        ssl_certificate_key /etc/tls/key.pem
-        access_log /var/log/nginx/finditgeek-presentation_access_log;
-        error_log /var/log/nginx/finditgeek-presentation_error_log;
-        
-        location / {
-            proxy_pass http://finditgeek:80
-        }
+server {
+    listen 443 ssl;
+    ssl_certificate     /etc/tls/cert.pem;
+    ssl_certificate_key /etc/tls/key.pem;
+    access_log /var/log/nginx/finditgeek-presentation_access_log;
+    error_log /var/log/nginx/finditgeek-presentation_error_log;
+
+    location / {
+        proxy_pass http://finditgeek;
     }
+}
+server {
+    listen 80;
+    access_log /var/log/nginx/finditgeek-presentation_access_log;
+    error_log /var/log/nginx/finditgeek-presentation_error_log;
+
+    return 301 https://10.10.10.10;
 }
 EOF'
