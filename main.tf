6d53dab69647cd1ae86e70cfea66a321e90f98a0
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-02-02 15:33:15 +0700

    update provisioning file

--- main.tf
+++ main.tf
@@ -76,6 +76,14 @@ resource "aws_security_group" "finditgeek_presentation_loadbalancer" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
--- provisioning_webserver.sh
+++ provisioning_webserver.sh
@@ -1,4 +1,25 @@
 #!bin/bash
 
-#Update repositori
+#Update repository and Upgrade OS
+sudo apt update
+sudo apt upgrade -y
+
+#Install Docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+#Build and run simple web app on Docker
+
+git clone https://github.com/jessie-txt/simple-webapp.git
+cd simple-webapp
+docker build . -t simple-webapp:latest
