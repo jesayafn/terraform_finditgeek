ab8b5f79ac8c08b9ede92fea1b652207ff095aa1
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-02-02 18:26:11 +0700

    fix

--- provisioning_webserver.sh
+++ provisioning_webserver.sh
@@ -6,9 +6,8 @@ sudo apt upgrade -y
 
 #Install and configure Docker
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo groupadd docker
